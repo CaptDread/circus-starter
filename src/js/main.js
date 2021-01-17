0d5069ed4e4f85946a7102d121c292e1adfccb83
CaptDread <domlycaon@gmail.com>
2021-01-17 12:31:11 -0500

    tooltipCommit
    latest commit for tooltip programs.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,52 @@
-console.log(`Hello World from main.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`)
+"use strict";
 
-const tooltips = new TooltipClass(`.tooltip`)
+var tooltips = new TooltipClass(".tooltip");
+var tipBase = document.querySelectorAll(".tooltip");
+var bkgd = document.querySelector(`.foreground`)
+
+
+var _loop = function _loop(z) {
+  tipBase[z].addEventListener("mouseover", function (event) {
+    var tipData = tipBase[z].attributes[1].value;
+    var tipDiv = document.createElement("div");
+    tipDiv.classList.add("tipDiv", z);
+    console.log("top", event.pageY);
+    console.log("left", event.pageX);
+    console.log("*top", event.pageY / 100 * 20 - 35);
+    console.log("*left", event.pageX / 100 * 20 - 160);
+    tipDiv.style.background = "rgba(0, 0, 0, .85)";
+    tipDiv.style.width = "fit-content";
+    tipDiv.style.padding = "1px 20px";
+    tipDiv.style.borderRadius = "25%";
+    tipDiv.style.position = "absolute";
+    tipDiv.style.left = event.pageX / 100 * 20 - 168 + `%`;
+    tipDiv.style.top = event.pageY / 100 * 20 - 45 + (z * 10) + `%`;
+    tipDiv.style.zIndex = "1";
+    tipDiv.style.color = 'white';
+    tipDiv.style.display = "flex";
+    tipDiv.style.flexDirection = "column";
+    tipDiv.style.alignItems = "center";
+    bkgd.appendChild(tipDiv);
+    var tip = document.createElement("p");
+    tip.innerText = tipData;
+    tipDiv.appendChild(tip);
+    var arrow = document.createElement("div");
+    arrow.style.background = "rgba(0, 0, 0, .75)";
+    arrow.style.width = "1.3em";
+    arrow.style.height = "1.3em";
+    arrow.style.transform = "rotate(45deg)";
+    arrow.style.position = "relative";
+    arrow.style.marginBottom = "-5%";
+    arrow.style.zIndex = `0`;
+    tipDiv.appendChild(arrow);
+  });
+  tipBase[z].addEventListener("mouseout", function () {
+    var tipDel = document.querySelector(`.tipDiv`, z);
+    tipDel.remove();
+  });
+};
+
+for (var z = 0; z < tipBase.length; z++) {
+  _loop(z);
+}
+//# sourceMappingURL=main.js.map
--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,68 +1,99 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
 /**
  * tooltip.js it makes a tooltip!
 * parameters:
 *   - seletor: the DOM seector that will get turned into a hoverable tooltip trigger.
 *   - content: the 
-**/ 
+**/
 
-const TooltipIIFE = (function() {
 
-    var _selector
-    var _content
+var TooltipIIFE = function () {
+  var _selector;
 
-    function init(selector, content){
-        console.log(`hi, i am TooltipIIFE.init`, selector, content)
-        _selector = selector
-        this.print()
+  var _content;
+
+  function init(selector, content) {
+    console.log("hi, i am TooltipIIFE.init", selector, content);
+    _selector = selector;
+    this.print();
+  }
+
+  print = function print() {
+    console.log("printing selector", _selector);
+  };
+
+  function getSelector() {
+    console.log("this?", _selector);
+    return this.selector;
+  }
+
+  return {
+    init: init,
+    getSelector: getSelector
+  };
+}();
+
+var TooltipClass = /*#__PURE__*/function () {
+  function TooltipClass(selector, content) {
+    _classCallCheck(this, TooltipClass);
+
+    console.log("hi i am tooltip class instance", selector, content);
+    this.selector = selector;
+    this.content = content;
+    this.setup();
+  }
+
+  _createClass(TooltipClass, [{
+    key: "setup",
+    value: function setup() {
+      console.log("setting up tooltip elements");
+      var tooltipElements = document.querySelectorAll(this.selector);
+      tooltipElements.forEach(function (element) {
+        var content = element.dataset[".tooltip"];
+        console.log("adding content", content, "to element", element.textcontent);
+      });
+    } // Getters
+
+  }, {
+    key: "getSelector",
+    value: function getSelector() {
+      return this.selector;
     }
+  }, {
+    key: "getContent",
+    value: function getContent() {
+      return this.content;
+    } // Setters
 
-    print = () => {
-        console.log(`printing selector`, _selector)
+  }, {
+    key: "setSelector",
+    value: function setSelector(newSelector) {
+      this.selector = newSelector;
     }
+  }]);
 
-    function getSelector(){
-        console.log(`this?`, _selector)
-        return this.selector
-    }
-
-    return {
-        init: init,
-        getSelector: getSelector
-    }
-}())
-
-class TooltipClass{
-    constructor(selector, content) {
-        console.log(`hi i am tooltip class instance`, selector, content)
-        this.selector = selector
-        this.content = content
-
-        this.setup()
-    }
-
-    setup(){
-        console.log(`setting up tooltip elements`)
-        const tooltipElements = document.querySelectorAll(this.selector)
-        tooltipElements.forEach(function (element) {
-            const content = element.dataset[`.tooltip`]
-            console.log(`adding content`, content, `to element`, element.textcontent)
-        })
-    }
-
-    // Getters
-    getSelector(){
-        return this.selector
-    }
-
-    getContent(){
-        return this.content
-    }
-
-
-    // Setters
-    setSelector(newSelector){
-        this.selector = newSelector
-    }
-
-
-}
+  return TooltipClass;
+}();
+//# sourceMappingURL=tooltip.js.map
